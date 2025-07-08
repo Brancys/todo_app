238b620c657e1b914e7666a13dac1b5d0537dc21
Brancys <brancysb@uninorte.edu.co>
2025-07-07 19:40:41 -0500

    Implemented class for task management with CRUD operations

---
+++ lib/data/task_database.dart
@@ -0,0 +1,69 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/task.dart';
+
+class TaskDatabase {
+  static final TaskDatabase _instance = TaskDatabase._internal();
+  factory TaskDatabase() => _instance;
+
+  TaskDatabase._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'tasks.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        isCompleted INTEGER NOT NULL
+      )
+    ''');
+  }
+
+  Future<List<Task>> getTasks() async {
+    final db = await database;
+    final result = await db.query('tasks', orderBy: 'id DESC');
+    return result.map((map) => Task.fromMap(map)).toList();
+  }
+
+  Future<int> insertTask(Task task) async {
+    final db = await database;
+    return await db.insert('tasks', task.toMap());
+  }
+
+  Future<int> updateTask(Task task) async {
+    final db = await database;
+    return await db.update(
+      'tasks',
+      task.toMap(),
+      where: 'id = ?',
+      whereArgs: [task.id],
+    );
+  }
+
+  Future<int> deleteTask(int id) async {
+    final db = await database;
+    return await db.delete(
+      'tasks',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
