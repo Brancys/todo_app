85ad195b402e85a8486b34fa0f1ca96755da3359
Brancys <brancysb@uninorte.edu.co>
2025-07-07 19:32:25 -0500

    Created Task model for app

---
+++ lib/models/task.dart
@@ -0,0 +1,35 @@
+class Task {
+  final int? id;
+  final String title;
+  final bool isCompleted;
+
+  Task({
+    this.id,
+    required this.title,
+    this.isCompleted = false,
+  });
+
+  Task copyWith({int? id, String? title, bool? isCompleted}) {
+    return Task(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'isCompleted': isCompleted ? 1 : 0,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'],
+      title: map['title'],
+      isCompleted: map['isCompleted'] == 1,
+    );
+  }
+}
